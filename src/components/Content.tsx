38cdd04a0380dde25f8c3efeb83fa95fadb30307
joaolobao380 <victor_jv380@hotmail.com>
2021-03-28 03:05:25 -0300

    completed

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,45 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard';
+
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface AllFunctions {
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+}
+
+
+
+export function Content(props: AllFunctions) {
+  return(
+    <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {props.selectedGenre.name}</span></span>
+    </header>
+  
+    <main>
+      <div className="movies-list">
+        {props.movies.map(movie => (
+          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+       ))} 
+      </div>
+    </main>
+  </div>
+  )
+ 
 }
